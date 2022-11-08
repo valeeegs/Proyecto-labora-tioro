3b360f7948eb11430ffcdb5b14dfc514ac323a27
Phoebe <107218075+valeeegs@users.noreply.github.com>
2022-11-07 22:07:52 -0400

    Add files via upload

---
+++ Ingrediente.java
@@ -0,0 +1,16 @@
+
+public class Ingrediente {
+	private String nombre;
+	private int cantidad;
+	private float precio;
+	
+	public Ingrediente(){
+		nombre = "Azúcar";
+		cantidad = 500;
+		precio = 20.0f;
+	}
+	
+	public void mostrar(){
+		System.out.println("Nombre");
+	}
+}
---
+++ Producto.java
@@ -0,0 +1,31 @@
+
+public class Producto {
+	private String nombre;
+	private int canIngredientes;
+	private Ingrediente[] ingredientes = new Ingrediente[50];
+	private int tiempoProd;
+	private int unidadesPorEmpaque;
+	
+	public Producto(){
+		nombre = "Baek";
+		canIngredientes = 3;
+		ingredientes[0] = new Ingrediente();
+		ingredientes[1] = new Ingrediente();
+		ingredientes[2] = new Ingrediente();
+		tiempoProd = 3;
+		unidadesPorEmpaque = 3;
+	}
+	
+	public void mosrtar(){
+		System.out.println("------ DATOS PRODUCTO -----"); 
+		System.out.println("Nombre: " + nombre); 
+		System.out.println("Cantidad Ingredientes: " + canIngredientes); 
+		System.out.println("NOMBRE \t CANTIDAD \t PRECIO"); 
+		for(int i=0; i<canIngredientes; i++) {
+			ingredientes[i].mostrar();
+		}
+		System.out.println("Tiempo prod: " + tiempoProd); 
+		System.out.println("Unidades x Empaque: " + unidadesPorEmpaque); 
+		System.out.println("----------------------------"); 
+	}
+}
