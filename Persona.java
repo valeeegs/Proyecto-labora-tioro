895594e0f09edc9aad450118e9b6811de897523d
YamiSolar <117697680+YamiSolar@users.noreply.github.com>
2022-11-07 23:18:42 -0400

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,23 @@
+
+public class Persona {
+	protected String nombre;
+	protected int ci;
+	protected int edad;
+	
+	public Persona(){
+		nombre = "Abel Valencia";
+		ci = 7397416;
+		edad = 29;
+	}
+	
+	public Persona(String nombre, int ci, int edad) {
+		this.nombre = nombre;
+		this.ci = ci;
+		this.edad = edad;
+	}
+	
+	public void mostrar(){
+		System.out.println(nombre + "\t\t" + ci + "\t\t" + edad);
+	}
+	
+}
