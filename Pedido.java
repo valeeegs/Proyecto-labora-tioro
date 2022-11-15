1bb4d46474b5e32ec2efbcb454f459208ada8998
Phoebe <107218075+valeeegs@users.noreply.github.com>
2022-11-14 23:54:11 -0400

    actu

--- Pedido.java
+++ Pedido.java
@@ -1,35 +1,54 @@
+import java.util.Scanner;
 
 public class Pedido implements java.io.Serializable{
 	private int idpedidos;
 	private int cantidadProd;
-	private String[][] prodComprados = new String[50][3];
+	private String[][] prodComprados = new String[50][2];
+	private double[] precioProd = new double[50];
 	
 	public Pedido(){
 		idpedidos = 127;
 		cantidadProd = 3;
-		prodComprados[0][0] = "a"; prodComprados[0][1] = "2"; prodComprados[0][2] = "";
-		prodComprados[1][0] = "b"; prodComprados[1][1] = "1"; prodComprados[1][2] = "";
-		prodComprados[2][0] = "a"; prodComprados[2][1] = "1"; prodComprados[2][2] = "";
+		prodComprados[0][0] = "a"; prodComprados[0][1] = "2"; 
+		prodComprados[1][0] = "b"; prodComprados[1][1] = "1"; 
+		prodComprados[2][0] = "a"; prodComprados[2][1] = "1"; 
 	}
 	
-	public Pedido(int idpedidos, int cantidadprod){
+	/*public Pedido(int idpedidos, int cantidadprod){
 		this.idpedidos = idpedidos;
 		this.cantidadProd = cantidadprod;
 		
+	}*/
+	
+	public void leer() {
+		Scanner read = new Scanner(System.in);
+		System.out.println("Ingrese los datos del pedido: ");
+		System.out.print("ID: ");
+		idpedidos = read.nextInt();
+		read.nextLine();
+		System.out.print("Cantidad de productos: ");
+		cantidadProd = read.nextInt();
+		read.nextLine();
+		for(int i=0; i<cantidadProd; i++) {
+			System.out.print("Nombre producto " + (i+1));
+			prodComprados[i][0] = read.nextLine();
+			System.out.print("Cantidad producto " + (i+1));
+			prodComprados[i][1] = read.nextLine();
+		}
 	}
 	
 	public void ponerPrecio(Inventario inv) {
 		double precio;
 		for(int i=0; i<cantidadProd; i++) {
 			precio = Double.parseDouble(prodComprados[i][1]) * inv.getPrecio(prodComprados[i][0]);
-			prodComprados[i][2] = String.valueOf(precio);
+			precioProd[i] = precio;
 		}
 	}
 	
 	public void mostrar(){
 		System.out.println(idpedidos + cantidadProd);
 		for(int i=0; i<cantidadProd; i++) {
-			System.out.println("\t" + prodComprados[i][0] + "\t" + prodComprados[i][1] + "\t" + prodComprados[i][2] + "\t");
+			System.out.println("\t" + prodComprados[i][0] + "\t" + prodComprados[i][1] + "\t" + precioProd[i] + "\t");
 		}
 	}
 	// use cases to compare and add the price of each product 
@@ -45,7 +64,7 @@ public class Pedido implements java.io.Serializable{
 	public double costoTotal(Cliente c) {
 		double costo = 0;
 			for(int i=0; i<cantidadProd; i++) {
-				costo += Double.parseDouble(prodComprados[i][2]);
+				costo += precioProd[i];
 			} if(c.getFrecuencia() > 5) { // descuento por frecuencia
 				costo -= descuento(costo);
 			} if(c.isDejarPropina()) { // si confirmó dejar porpina 
